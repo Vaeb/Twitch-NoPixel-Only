82a3bba46040728aa5bf70f06a4d94a0b988ec98
Adam Johnson <vaeb@hey.com>
2021-02-18 16:00:33 +0000

    minor

--- src/js/foreground.js
+++ src/js/foreground.js
@@ -5,7 +5,7 @@
 
 console.log('[TNO] Loading Twitch NoPixel Only...');
 
-const allowAll = true;
+const allowAll = false;
 
 const getStorage = (key, defaultVal = undefined) => new Promise((resolve) => {
     const useDefault = defaultVal !== undefined;
