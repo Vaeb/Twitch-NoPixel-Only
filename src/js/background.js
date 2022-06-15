61e4a99164a78e9b079ef6dd3018f1d7d3ffe8bc
Adam Johnson <vaeb@hey.com>
2022-06-15 14:20:58 +0100

    Firefox version

--- src/js/background.js
+++ src/js/background.js
@@ -16,7 +16,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 });
 
-chrome.action.onClicked.addListener((activeTab) => {
+chrome.browserAction.onClicked.addListener((activeTab) => {
     const newURL = 'https://www.twitch.tv/directory/game/Grand%20Theft%20Auto%20V';
     chrome.tabs.create({ url: newURL });
 });
